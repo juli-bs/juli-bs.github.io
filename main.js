dca820c8ac00249a3534af1423f14e72e0389a28
Julian Gürtler <j.guertler@tu-braunschweig.de>
2023-03-22 20:51:59 +0100

    Rüberkopiert

---
+++ main.js
@@ -0,0 +1,45 @@
+let todos = [];
+todos = JSON.parse(localStorage.getItem('todos'));
+
+const inputfield = document.getElementById("inputfield");
+const todosection = document.getElementById("todosection");
+
+
+function addTodo() {
+   if(inputfield.value != "") {
+      todos.push(inputfield.value);
+      showTodos();
+      inputfield.value = "";
+   }
+}
+
+function removeTodo(i) {
+   todos.splice(i, 1);
+   showTodos();
+}
+
+function showTodos() {
+   let text = "";
+   for(let i = 0; i < todos.length; i++) {
+      if(i == 0) {
+         text += "<div id='todo' style='border-top: 2px solid #050022;border-bottom: 2px solid #050022;'>" + todos[i];
+      } else {
+         text += "<div id='todo' style='border-bottom: 2px solid #050022;'>" + todos[i];
+      }
+      text += "<button id='removebutton' onclick='removeTodo(" + i + ");'><span>Remove</span></button>";
+      text += "</div>";
+   }
+
+   todosection.innerHTML = text;
+   localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// alternative to add-button
+inputfield.addEventListener("keyup", function(event) {
+   if (event.keyCode === 13) {
+   // Code, der ausgeführt wird, wenn die Enter-Taste gedrückt wird
+      addTodo();
+   }
+});
+
+showTodos();
